61e5c43ddf03e1b288d7f3f879162b2de1262d12
Jack Linde <garslade359@gmail.com>
2018-11-06 01:19:39 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -29,13 +29,12 @@ self.addEventListener('fetch', function (e) {
         return fetch(e.request)
       }
 
-      // You can omit if/else for console.log & put one line below like this too.
+      // You can omit if/else for console.log & put one line below here too.
       // return request || fetch(e.request)
     })
   )
 })
 
-// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -45,18 +44,13 @@ self.addEventListener('install', function (e) {
   )
 })
 
-// Delete outdated caches
 self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
+    caches.keys().then(function(keyList) {
       var cacheWhitelist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX)
       })
-      // add current cache name to white list
       cacheWhitelist.push(CACHE_NAME)
-
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
           console.log('deleting cache : ' + keyList[i] )
